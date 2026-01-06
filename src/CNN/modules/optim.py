0d0559e7ce92e77b234e3922006cc7cfb72b862c
Nasty_Hedgeh0g <niazimohammada@gmail.com>
2026-01-06 20:08:47 +0330

    added SGD to optim list

--- src/CNN/modules/optim.py
+++ src/CNN/modules/optim.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+import numpy as np
 
 __all__ = [
-    
+    "SGD"
 ]
 
 class _optimizer(ABC):
@@ -10,7 +11,7 @@ class _optimizer(ABC):
         self.iteration = 0
 
     @abstractmethod
-    def updatePAram(self):
+    def updateParam(self, layer):
         pass
 
     def step(self):
@@ -19,3 +20,40 @@ class _optimizer(ABC):
     def reset(self):
         self.iteration = 0
 
+class SGD(_optimizer):
+    def __init__(self, _learningRate, momentum=0.0):
+        super().__init__(_learningRate)
+        self.momentum = momentum
+        self.velocity = {}
+        self.wUpdated = False
+        self.bUpdated = False
+    
+    def updateParam(self, layer):
+        self.wUpdated = False
+        self.bUpdated = False
+
+        layerID = id(layer)
+
+        gradW, gradB = layer.getGradient()
+
+        if layerID not in self.velocity:
+            self.velocity[layerID] = {
+                'weights' : np.zeros_like(gradW) if gradW is not None else None,
+                'biases'  : np.zeros_like(gradW) if gradB is not None else None 
+            }
+        
+        if gradW is not None:
+            velW = self.velocity[layerID]['weights']
+            velW = self.momentum * velW - self.lr * gradW
+            self.velocity[layerID]['weights'] = velW
+            self.wUpdated = True
+        if gradB is not None:
+            velB = self.velocity[layerID]['biases']
+            velB = self.momentum * velW - self.lr * gradB
+            self.velocity[layerID]['biases'] = velB
+            self.bUpdated = True
+
+
+        if self.bUpdated or self.wUpdated:
+            layer.updateParam(self.velocity[layerID]['weights'], self.velocity[layerID]['biases'])
+        self.step()
